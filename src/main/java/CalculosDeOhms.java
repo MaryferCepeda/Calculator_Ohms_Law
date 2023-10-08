238d753d88756f54f274c53469219c8d180960ff
MaryferCepeda <maryfer1254@gmail.com>
2023-10-08 15:23:00 -0600

    Actualizacion de las funciones

--- src/main/java/CalculosDeOhms.java
+++ src/main/java/CalculosDeOhms.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JTextField;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,10 +10,22 @@
  *
  * @author HOGAR
  */
-public class CalculosDeOhms extends GUI {
-    float inputVoltaje, inputCoriente, inputIntensidad; 
+public class CalculosDeOhms{
+    float vol, cor, inten;  
     
-    public
+    float calcularVoltaje(float cor, float inten){
+        this.cor=cor; 
+        this.inten=inten;
+        vol=cor*inten; 
+        return vol; 
+    }
+    
+    float calcularCorriente(float vol, float inten){ 
+        this.vol=vol; 
+        this.inten=inten; 
+        cor=
+    }
+    
             
 }
 
