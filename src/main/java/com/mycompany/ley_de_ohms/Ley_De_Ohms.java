5092cc01da08d9d9e812d7ee98c0e9e6c08dc486
MaryferCepeda <maryfer1254@gmail.com>
2023-10-08 14:55:16 -0600

    Creacion del proyecto y creacion de gui

---
+++ src/main/java/com/mycompany/ley_de_ohms/Ley_De_Ohms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ley_de_ohms;
+
+/**
+ *
+ * @author HOGAR
+ */
+public class Ley_De_Ohms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
